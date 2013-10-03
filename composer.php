28cc5549f36058be6501dc4dc02da9b123247df5
André Filipe <andre.r.flip@gmail.com>
2013-10-03 21:06:05 +0100

    Added composer config

---
+++ composer.php
@@ -0,0 +1,21 @@
+{
+    "name": "masnathan/ogravatar",
+    "type": "library",
+    "description": "Simple class that provides easy gravatar integration.",
+    "keywords": ["gravatar","image","profile"],
+    "homepage": "https://github.com/ReiDuKuduro/oGravatar",
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "André Filipe",
+            "email": "andre.r.flip@gmail.com",
+            "role": "Developer"
+        }
+    ],
+    "require": {
+        "php": ">=5.3.0"
+    },
+    "autoload": {
+        "files": ["oGravatar.php"]
+    }
+}
